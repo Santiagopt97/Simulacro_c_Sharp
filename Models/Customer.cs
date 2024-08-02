6c16f4e011826bc535349dc84c750f462e04617c
santiagopt97 <santiagopineda99@gmail.com>
2024-08-02 07:28:51 -0500

    Feat: cree la clase customer con sus atributos y metodos

---
+++ Models/Customer.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simulacro_c_Sharp.Models
+{
+    public class Customer : User
+    {
+        public string MembershipLevel { get; set; }
+        public string PreferredPaymentMethod { get; set; }
+
+        public Customer(string name, string lastName, string typeDocument, string identificationNumber, DateOnly birthDate, string email, string phone, string address, string membershipLevel, string preferredPaymentMethod) : base(name, lastName, typeDocument, identificationNumber, birthDate, email, phone, address)
+        {
+            MembershipLevel = membershipLevel.ToLower();
+            PreferredPaymentMethod = preferredPaymentMethod.ToLower();
+        }
+
+        public void UpdateMembershipLevel(){
+            Console.WriteLine("Please write your new membership level");
+            var newMembershipLevel = Console.ReadLine().ToLower();
+            MembershipLevel = newMembershipLevel;
+            Console.WriteLine($"Your new membership level is: {MembershipLevel}");
+        }
+    }
+}
