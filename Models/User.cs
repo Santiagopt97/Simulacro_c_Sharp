195b80a65f01d1d12232f88f21f7fa655e149fe1
santiagopt97 <santiagopineda99@gmail.com>
2024-08-02 07:13:42 -0500

    Feat: cree la clase persona con sus atributos y metodos

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simulacro_c_Sharp.Models
+{
+    public class User
+    {
+        
+    }
+}
